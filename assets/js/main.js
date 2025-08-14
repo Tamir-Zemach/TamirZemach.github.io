952e8e8b5a5ff15a5fb0a3e9bd131cec926caf6a
Tamir Zemach <sattheti1@gmail.com>
2025-08-14 15:47:27 +0300

    Working Email

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,29 +6,55 @@
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+    var $window = $(window),
+        $body = $('body'),
+        $nav = $('#nav');
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:  [ '1281px',  '1680px' ],
-			large:   [ '981px',   '1280px' ],
-			medium:  [ '737px',   '980px'  ],
-			small:   [ null,      '736px'  ]
-		});
+    // Breakpoints.
+    breakpoints({
+        xlarge: [ '1281px', '1680px' ],
+        large: [ '981px', '1280px' ],
+        medium: [ '737px', '980px' ],
+        small: [ null, '736px' ],
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function() {
+        window.setTimeout(function() {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Scrolly.
-		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
-		});
+    // Scrolly.
+    $('#nav a, .scrolly').scrolly({
+        speed: 1000,
+        offset: function() { return $nav.height(); }
+    });
+
+    // Formspree AJAX submission
+    $('#contact-form').on('submit', function(e) {
+        e.preventDefault();
+
+        const form = e.target;
+        const data = new FormData(form);
+
+        fetch('https://formspree.io/f/mjkozwkz', {
+            method: 'POST',
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                form.reset();
+                $('#popup').addClass('show');
+            } else {
+                alert('Oops! Something went wrong.');
+            }
+        });
+    });
+	
+
 
 })(jQuery);
+
