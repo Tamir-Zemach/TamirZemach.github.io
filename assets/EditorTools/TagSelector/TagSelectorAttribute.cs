c89c207eb55cb5d6afa95399d0a5fe99de50ed05
Tamir Zemach <sattheti1@gmail.com>
2025-11-18 15:22:55 +0200

    Added Tag selector

---
+++ assets/EditorTools/TagSelector/TagSelectorAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BackEnd.Project_inspector_Addons
+{
+    public class TagSelectorAttribute : PropertyAttribute
+    {
+        public string Tooltip { get; }
+
+        public TagSelectorAttribute(string tooltip = "")
+        {
+            Tooltip = tooltip;
+        }
+    }
+}
---
+++ assets/EditorTools/TagSelector/TagSelectorPropertyDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace BackEnd.Project_inspector_Addons
+{
+#if UNITY_EDITOR
+    // Custom drawer for fields marked with [TagSelectorAttribute].
+    [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+    public class TagSelectorPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var tagSelector = (TagSelectorAttribute)attribute;
+            var dynamicLabel = new GUIContent(label.text, tagSelector.Tooltip);
+
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                EditorGUI.BeginProperty(position, dynamicLabel, property);
+                position = EditorGUI.PrefixLabel(position, label);
+
+                string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+                int index = Array.IndexOf(tags, property.stringValue);
+                index = EditorGUI.Popup(position, index, tags);
+
+                property.stringValue = index >= 0 ? tags[index] : "";
+                EditorGUI.EndProperty();
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property, dynamicLabel);
+            }
+        }
+    }
+#endif
+}
